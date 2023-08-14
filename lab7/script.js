73fd9755a5f9a6604e453d3f2119b1f85c1bb1c7
Robel-Hayle <robel.hayle96@gmail.com>
2023-08-13 19:10:11 -0500

    my clean screen circles

--- lab7/script.js
+++ lab7/script.js
@@ -8,16 +8,26 @@ spreadCirclesButton.addEventListener('click', spreadCircles);
 function createSingleCircle() {
   const circleWidth = parseInt(document.getElementById('circleWidth').value);
   const growthAmount = parseInt(document.getElementById('growthAmount').value);
+  const maxCircleSize = circleWidth * 5; // Define maximum size
   const growRate = parseInt(document.getElementById('growRate').value);
+  
+  const randomColor = getRandomColor();
 
   const circle = document.createElement('div');
   circle.className = 'circle';
   circle.style.width = circleWidth + 'px';
   circle.style.height = circleWidth + 'px';
+  circle.style.backgroundColor = randomColor;
 
   circle.addEventListener('click', function () {
     circleContainer.removeChild(circle);
   });
+
+  const x = Math.random() * (circleContainer.clientWidth - circleWidth);
+  const y = Math.random() * (circleContainer.clientHeight - circleWidth);
+
+  circle.style.left = x + 'px';
+  circle.style.top = y + 'px';
 
   circleContainer.appendChild(circle);
 
@@ -26,18 +36,26 @@ function createSingleCircle() {
     currentWidth += growthAmount;
     circle.style.width = currentWidth + 'px';
     circle.style.height = currentWidth + 'px';
-  }, growRate);
 
-  // Stop growing after 10 seconds (10000 milliseconds)
-  setTimeout(() => {
-    clearInterval(interval);
-  }, 10000);
+    if (currentWidth >= maxCircleSize) {
+      clearInterval(interval);
+    }
+  }, growRate);
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 function spreadCircles() {
   const numCircles = parseInt(document.getElementById('numCircles').value);
   
   for (let i = 0; i < numCircles; i++) {
-    setTimeout(createSingleCircle, i * 500); // Delay creation for better spread
+    setTimeout(createSingleCircle, i * 100); // Delay creation for better spread
   }
 }
